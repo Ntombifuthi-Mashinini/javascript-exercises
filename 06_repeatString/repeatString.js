ffe2e8e9f5f90797b4087a392e250da367510998
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-11-09 21:25:09 +0200

    Fixed repeatString logic and added passing Jest tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+  if (times < 0) return "ERROR";
+  let results = '';
+  for (let i = 0; i < times; i++) {
+    results += str;
+  }
+  return results;
 };
+
+module.exports = repeatString;
 
 // Do not edit below this line
 module.exports = repeatString;
